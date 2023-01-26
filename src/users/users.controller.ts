0844816190b9bf6f3668701d04a0c1cd2c396105
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-01-26 14:36:30 +0200

    50. Realize usersService method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@ApiTags('User')
+@ApiTags('Users')
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    this.usersService.create(body.email, body.password);
   }
 }
