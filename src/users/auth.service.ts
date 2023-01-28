1e5d5b7b358c753826deb97d80b43e8bce60344c
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-01-28 13:01:06 +0200

    72. Started realize signup method

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common/exceptions';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+  async signup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (!users.length) {
+      throw new BadRequestException('email in use');
+    }
+  }
+
+  signin() {}
 }
