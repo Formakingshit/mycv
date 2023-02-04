74568fd616864c7a472313f90a4e35bfce83c896
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-02-04 13:23:41 +0200

    99. Ensured throwing an error when signin is called with an unused email

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
@@ -48,6 +48,14 @@ describe('AuthService', () => {
 
       await expect(service.signup('test@test.com', 'password')).rejects.toThrow(
         BadRequestException,
+      );
+    });
+  });
+
+  describe('signin', () => {
+    it('throws if signin is called with an unused email', async () => {
+      await expect(service.signin('test@test.com', 'password')).rejects.toThrow(
+        NotFoundException,
       );
     });
   });
