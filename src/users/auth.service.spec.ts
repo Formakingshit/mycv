b5b67dc15a3665e2aeeda4cf9a803f608eb74d4e
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-02-04 13:12:41 +0200

    97. Covered signup method

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,15 +1,18 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password }),
+        Promise.resolve({ id: 1, email, password } as User),
     };
 
     const module = await Test.createTestingModule({
@@ -29,12 +32,23 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  it('creates a new user with a salted and hashed password', async () => {
-    const user = await service.signup('test@test.com', 'password');
-    expect(user.password).not.toEqual('password');
+  describe('signup', () => {
+    it('creates a new user with a salted and hashed password', async () => {
+      const user = await service.signup('test@test.com', 'password');
+      expect(user.password).not.toEqual('password');
 
-    const [salt, hash] = user.password.split('.');
-    expect(salt).toBeDefined();
-    expect(hash).toBeDefined();
+      const [salt, hash] = user.password.split('.');
+      expect(salt).toBeDefined();
+      expect(hash).toBeDefined();
+    });
+
+    it('throws an error if user signs up with email that is in use', async () => {
+      fakeUsersService.find = () =>
+        Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+
+      await expect(service.signup('test@test.com', 'password')).rejects.toThrow(
+        BadRequestException,
+      );
+    });
   });
 });
