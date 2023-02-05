68c5389d5f4419070585fa0e2ec9afc8c6483bfd
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-02-05 14:36:11 +0200

    111. Exploring with e2e tests for auth

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/signup (POST)', () => {
+    const sendEmail = 'test@test.com';
+
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: sendEmail, password: 'password' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(sendEmail);
+      });
+    // .get('/')
+    // .expect(200)
+    // .expect('Hello World!');
+  });
+});
