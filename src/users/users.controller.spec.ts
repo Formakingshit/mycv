8541ef2a554ad7f57734a08157cbadb07d08601c
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-02-05 13:54:08 +0200

    107. Covered findAllUsers and findUser

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -53,5 +54,26 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAllUsers', () => {
+    it('returns a list of users with the given email', async () => {
+      const users = await controller.findAllUsers('test@test.com');
+      expect(users.length).toEqual(1);
+      expect(users[0].email).toEqual('test@test.com');
+    });
+  });
+
+  describe('findUser', () => {
+    it('returns a single user with the given id', async () => {
+      const user = await controller.findUser('1');
+      expect(user).toBeDefined();
+    });
+
+    it('throws an error if user with given id is not found', async () => {
+      fakeUsersService.findOne = () => null;
+
+      await expect(controller.findUser('2')).rejects.toThrow(NotFoundException);
+    });
   });
 });
