5562fb383b06bebfd892c77bc890569ad3416699
Vladyslav Herasymchuk <slaughternal@gmail.com>
2023-02-10 16:18:07 +0200

    143. Added admin guard

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -15,6 +15,7 @@ import { ReportsService } from './reports.service';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @ApiTags('Reports')
 @Controller('reports')
@@ -30,7 +31,7 @@ export class ReportsController {
   }
 
   @Patch('/:id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
